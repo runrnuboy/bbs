b863908067b1b3e756fd0bcad197ef365f8cc2c8
Seamile <lanhuermao@gmail.com>
2018-10-26 16:13:53 +0800

    1. 完成了用户登录 2. 完成了用户退出 3. 完成了用户个人信息页

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 
+from user.models import User
 from user.forms import RegisterForm
 
 
@@ -19,12 +20,33 @@ def register(request):
 
 
 def login(request):
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        nickname = request.POST.get('nickname')
+        password = request.POST.get('password')
+
+        # 取出用户数据
+        try:
+            user = User.objects.get(nickname=nickname)
+        except User.DoesNotExist:
+            return render(request, 'login.html', {'error': '用户不存在'})
+
+        # 检查用户密码
+        if check_password(password, user.password):
+            request.session['uid'] = user.id
+            request.session['nickname'] = user.nickname
+            request.session['avatar'] = user.icon.url
+            return redirect('/user/info/')
+        else:
+            return render(request, 'login.html', {'error': '密码错误'})
+    return render(request, 'login.html')
 
 
 def logout(request):
-    return render(request, 'logout.html', {})
+    request.session.flush()
+    return redirect('/')
 
 
 def user_info(request):
-    return render(request, 'user_info.html', {})
+    uid = request.session['uid']
+    user = User.objects.get(pk=uid)
+    return render(request, 'user_info.html', {'user': user})
