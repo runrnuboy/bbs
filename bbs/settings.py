4830c27dedb6415ebc2d73f493bae9e6e34991e0
Seamile <lanhuermao@gmail.com>
2018-10-26 11:17:57 +0800

    完成了帖子创建和阅读功能

--- bbs/settings.py
+++ bbs/settings.py
@@ -25,18 +25,16 @@ SECRET_KEY = '_xmob5+mg3*ry@jl2cxsa0(4+g3jjhgbxxl2v&k0!v0^+k(2^k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post',
 ]
 
 MIDDLEWARE = [
@@ -44,8 +42,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -60,8 +56,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
--- post/views.py
+++ post/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from post.models import Post
+
+
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title').strip()
+        content = request.POST.get('content').strip()
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%s' % post.id)
+    return render(request, 'create_post.html')
+
+
+def edit_post(request):
+    return render(request, 'edit_post.html', {})
+
+
+def read_post(request):
+    raise
+    post_id = int(request.GET.get('post_id'))
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'read_post.html', {'post': post})
+
+
+def delete_post(request):
+    return render(request, 'delete_post.html', {})
+
+
+def post_list(request):
+    return render(request, 'post_list.html', {})
